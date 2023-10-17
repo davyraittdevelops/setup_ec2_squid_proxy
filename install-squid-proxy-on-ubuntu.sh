f4df62e3fa34420a6b1b3509cd3286d0f4a61b3d
davyraittdevelops <141024641+davyraittdevelops@users.noreply.github.com>
2023-10-17 13:20:16 +0200

    Update install-squid-proxy-on-ubuntu.sh

--- install-squid-proxy-on-ubuntu.sh
+++ install-squid-proxy-on-ubuntu.sh
@@ -16,21 +16,47 @@ check_success
 sudo apt install tinyproxy -y
 check_success
 
-# Modify the TinyProxy configuration to allow your IP
-# Replace "your_ip_address" with your actual IP address
+# Modify the TinyProxy configuration to allow all IP addresses, set the port,
+# specify the user and group under which TinyProxy will run,
+# set the maximum number of clients, and set the number of servers to start
 sudo bash -c "cat > /etc/tinyproxy/tinyproxy.conf" << EOL
-User nobody
-Group nogroup
+User tinyproxy
+Group tinyproxy
 Port 8888
 Allow 0.0.0.0/0
 MaxClients 100
-MinSpareServers 5
-MaxSpareServers 20
 StartServers 10
 EOL
 check_success
-# Restart TinyProxy to apply the changes
-sudo /etc/init.d/tinyproxy restart
+
+# Create the tinyproxy user and group, if they don't already exist
+if ! id -u tinyproxy >/dev/null 2>&1; then
+    sudo addgroup --system tinyproxy
+    sudo adduser --system --no-create-home --ingroup tinyproxy tinyproxy
+fi
+check_success
+
+# Create a custom systemd service file for TinyProxy
+sudo bash -c "cat > /etc/systemd/system/tinyproxy.service" << EOL
+[Unit]
+Description=Tinyproxy lightweight HTTP Proxy
+After=network.target
+
+[Service]
+User=tinyproxy
+Group=tinyproxy
+ExecStart=/usr/sbin/tinyproxy -d
+
+[Install]
+WantedBy=multi-user.target
+EOL
+check_success
+
+# Reload systemd to recognize the new service file, then start TinyProxy
+sudo systemctl daemon-reload
+check_success
+
+sudo systemctl start tinyproxy
 check_success
 
 echo "Proxy setup completed. Configure your applications to use the proxy at $(curl -s ifconfig.me):8888"
